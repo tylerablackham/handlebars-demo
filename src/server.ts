e29389ceb0205816a33222f46e972b5e52e85440
Tyler Blackham <tab518@byu.edu>
2025-10-07 18:05:45 -0600

    feat: fill out demo pages

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,7 @@ fastify.register(view, {
   options: {
     partials: {
       header: "partials/header.hbs",
+      exclaim: "partials/exclaim.hbs"
     }
   }
 });
@@ -26,36 +27,74 @@ fastify.register(view, {
 fastify.get("/", async (req, reply) => {
   return reply.view("index.hbs", {
     title: "Fastify + Handlebars Demo",
+    home: true,
     message: "Welcome to your first Fastify + Handlebars page!",
   });
 });
 
 fastify.get('/simple-expressions', async(req, reply) => {
   return reply.view('simpleExpressions.hbs', {
-    title: 'Simple Expressions'
+    title: 'Simple Expressions',
+    header: 'Hello',
+    person: {
+      firstName: 'Foo',
+      lastName: 'Baz',
+      age: 35,
+    }
   })
 })
 
 fastify.get('/basic-helpers', async(req, reply) => {
   return reply.view('basicHelpers.hbs', {
-    title: 'Basic Helpers'
+    title: 'Basic Helpers',
+    header: 'Hello',
+    person: {
+      firstName: 'Foo',
+      lastName: 'Baz',
+      age: 35,
+    }
   })
 })
 
 fastify.get('/block-helpers', async(req, reply) => {
   return reply.view('blockHelpers.hbs', {
-    title: 'Block Helpers'
+    title: 'Block Helpers',
+    names: [
+      {first: 'Foo', last: 'Baz'},
+      {first: 'Bar', last: 'Baz'},
+      {first: 'Foobar'},
+    ]
   })
 })
 
 fastify.get('/partials', async(req, reply) => {
   return reply.view('partials.hbs', {
-    title: 'Partials'
+    title: 'Partials',
+    exclamation: 'Hello',
+    names: [
+      'foo',
+      'bar',
+      'baz'
+    ],
+    context: {
+      exclamation: 'Go Context'
+    }
   })
 })
 
 /**
- * Run the server!
+ * Define Handlebars helpers
+ */
+handlebars.registerHelper('loud', function(string: String):String {
+  return string.toUpperCase();
+})
+
+handlebars.registerHelper('append', function(s1: String, s2: String, addSpace:boolean = true):String {
+  return s1 + (addSpace ? ' ' : '') + s2;
+})
+
+/**
+ * Run the server
  */
 const start = async () => {
   try {
